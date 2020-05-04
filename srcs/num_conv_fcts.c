00ab95bf7b8e35581cd11edaee75e7cbe024177f
chlotte <charlottealleaume99@gmail.com>
2020-05-04 18:58:51 +0200

    corrected len calculation with d conv

--- srcs/num_conv_fcts.c
+++ srcs/num_conv_fcts.c
@@ -48,13 +48,8 @@ int	i_conv(va_list args, t_flags flg, char **buff, int *c_count)
 	len = ft_strlen(arg);
 	if (flg.align == 2 && len < flg.width && flg.prec < 0)
 		arg = ft_add_zeros(arg, flg.width - len);
-	else if (flg.prec > len)
-	{
-		if (i_tmp < 0)
-			arg = ft_add_zeros(arg, flg.prec - len + 1);
-		else
-			arg = ft_add_zeros(arg, flg.prec - len);
-	}
+	else if (flg.prec > (len = i_tmp < 0 ? len -1 : len))
+		arg = ft_add_zeros(arg, flg.prec - len);
 	if (flg.prec == 0 && ft_strcmp(arg, "0") == 0)
 		arg[0] = '\0';
 	if (flg.width > 0 && (len = ft_strlen(arg)) < flg.width)
